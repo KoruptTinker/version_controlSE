c865ad06c44e93ef17e5b7f7791ae261700fd718
KoruptTinker <pisces1237@gmail.com>
2020-01-22 20:17:43 +0000

    addded bubblesort using iterative method

--- test.java
+++ test.java
@@ -1,8 +1,33 @@
 import java.util.Scanner;
 
 public class test {
+    int size;
+    int[] arr = new int[size];
+
+    public int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < this.size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        System.out.println("First commit!");
-        System.out.print("Hello world");
+        Scanner sc = new Scanner(System.in);
+        test t = new test();
+        t.size = sc.nextInt();
+        for (int i = 0; i < t.size; i++) {
+            t.arr[i] = sc.nextInt();
+        }
+        sc.close();
+        t.arr = t.bubbleSort(t.arr);
+        for (int i = 0; i < t.size; i++) {
+            System.out.println(t.arr[i]);
+        }
     }
 }
